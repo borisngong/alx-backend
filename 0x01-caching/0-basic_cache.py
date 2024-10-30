1c9aee3c72905fca1c9875cae2d513077d206663
borisngong <borisngong@gmail>
2024-10-29 20:42:52 -0700

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/en python3
+"""
+
+"""
+from base_caching import BaseCaching
+class BasicCache(BaseCaching):
+    """ Basic Cache catching system without limit"""
+    
+    def put(self, key, item):
+        """ Add an item in the catche """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves an item from the cache by key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
