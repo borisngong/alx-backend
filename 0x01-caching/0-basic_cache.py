068156787aae620a86b7bb9d5766736c8a4a4145
borisngong <borisngong@gmail>
2024-10-29 21:13:54 -0700

    Basic Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,17 @@
-#!/usr/bin/en python3
-"""
+#!/usr/bin/env python3
+""" BasicCache module """
 
-"""
 from base_caching import BaseCaching
+
+
 class BasicCache(BaseCaching):
-    """ Basic Cache catching system without limit"""
-    
+    """ Basic caching system with no limit """
+
     def put(self, key, item):
-        """ Add an item in the catche """
+        """ Add an item in the cache """
         if key is not None and item is not None:
-            return
-        self.cache_data[key] = item
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieves an item from the cache by key"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
