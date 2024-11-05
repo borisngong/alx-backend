32e75045ea6ff371b4a9efaa7f79f6aca90457c7
borisngong <borisngong@gmail>
2024-11-05 13:01:48 -0800

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,13 +22,13 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     return render_template('2-index.html')
 
 
