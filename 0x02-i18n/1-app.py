e33795f512323b665186d7bc693a9c5a8e60fb0f
borisngong <borisngong@gmail>
2024-11-05 12:14:32 -0800

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Module for working with flask_Babel """
-
-
-from flask import Flask, render_template
+"""
+Module for working with flask, flask_babel
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
+
+
+class Config:
+    """
+    A class responsible for Flask Babel configuration
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 babel = Babel(app)
-
-
-class config:
-    LANGUAGES = ["en", "fr"]
-    # Default locale and timezone
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# Making config claa as apps configuration
-app.config.from_object(config)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def index():
-    """ """
-    return render_template('0-index.html')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
